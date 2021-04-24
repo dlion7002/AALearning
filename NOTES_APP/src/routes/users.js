baf3b1b06f51a686fe761d90d02f58025701fcab
dlion <jacobodleon@gmail.com>
2021-04-23 19:06:24 -0500

    .

--- NOTES_APP/src/routes/users.js
+++ NOTES_APP/src/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User')
 
 router.get('/users/singin', (req, res) => {
     res.render("users/singin");
@@ -9,9 +10,25 @@ router.get('/users/singup', (req, res) => {
     res.render("users/singup");
 })
 
-router.post("/user/singup", (req, res) => {
-    console.log(req.body)
-    res.send("Ok");
+router.post("/user/singup", async (req, res) => {
+    const { name, email, password, confirm_password} = req.body;
+    const errors = [];
+    if(name.length <= 0) {
+        errors.push("Please insert a name");
+    }
+    if (password != confirm_password) {
+        errors.push({text: "Passwords do not match"});
+    }
+    if (password.length < 4) {
+        errors.push({ text: "Password must be at least 4 characters"});
+    }
+    if(errors.length > 0) {
+        res.render("users/singup", {errors, name, email, password, confirm_password});
+    } else {
+        const newUser = new User({name, email, password})
+        newUser.password = await newUser.encryptPassword(password);
+        await newUser.save();
+    }
 })
 
 module.exports = router;
