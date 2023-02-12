3b97141f55f010afea4e41db423ad93d57e10173
dlion7002 <jacobodleon@gmail.com>
2023-02-11 19:27:31 -0500

    Changes
    Deleted some unnecessary files

---
+++ AAProblems/JavaScriptProblems/Mudae.js
@@ -0,0 +1,16 @@
+var names = promt("Insert names")
+
+function mudae(e) {
+    var res = [];
+    e.split(/\r?\n/)
+    
+    for (var i = 0; i < e.length; i++) {
+        
+    }
+
+    return e
+}
+
+
+
+console.log(mudae(names))
--- AProblems/JavaScriptProblems/Which Function Returns the Larger Number.js
+++
@@ -1,3 +0,0 @@
-function whichIsLarger(f, g) {
-	return f()>g()?'f':g()>f()?'g':'neither'
-}
